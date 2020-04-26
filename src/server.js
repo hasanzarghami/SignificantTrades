e8494184176a1538e09777f434a484f9b0de15d0
Kevin Rostagni <kevin.rostagni@gmail.com>
2020-04-26 17:19:42 +0200

    bugfix rate limiting

--- src/server.js
+++ src/server.js
@@ -311,6 +311,8 @@ class Server extends EventEmitter {
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 100 // limit each IP to 100 requests per windowMs
     })
+    
+    app.set('trust proxy', 1);
 
     //  apply to all requests
     app.use(limiter)
